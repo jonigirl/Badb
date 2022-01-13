e436fe3b743c38f1c4d97944c8bbd5a9218ce74e
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-14 03:38:31 +1000

    Update logging.py

--- cogs/logging.py
+++ cogs/logging.py
@@ -81,7 +81,7 @@ class logging(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.db = dbHandler(".badb.db")
+        self.db = dbHandler("/data/.logs.db")
         self.OPERATION_REACT_ADD = 'A'
         self.OPERATION_REACT_DELETE = 'D'
         self.OPERATION_MESSAGE = 'A'
@@ -149,8 +149,9 @@ class logging(commands.Cog):
 
 
 def check_folders():
-    if os.path.exists(".badb.db"):
+    if os.path.exists("data/.logs.db"):
         print("Database found...")
+        os.makedirs("data/.logs.db")
 
 
 def setup(bot):
