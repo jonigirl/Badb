4af5566550c56828721e8389f7b9cc44c8bb1384
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-14 05:47:47 +1000

    Update fuckoff.py

--- cogs/fuckoff.py
+++ cogs/fuckoff.py
@@ -12,12 +12,12 @@ class fuckoff(vbu.Cog):
         self.bot = bot
         self.foffmsg = ["Locating a fuck to give", "Looking for a fuck to give", "It is called KARMA and it is pronounced as haha fuck you", "You cannot imagine the immensity of the fuck I do not give", "I dont hate you but, lets put it this way. If I had a bucket of water and you were on fire, I would drink the bucket of water", "I hope you step on a lego", "Even with this wine you look ugly", "NO, FUCK YOU", "Dear karma, I have a list of people you missed. One of them is", "Attempting to give a fuck \n`PLEASE WAIT`"]
 
-    @vbu.commands(pass_context=True, name="foff", aliases=["fuckoff"])
+    @vbu.command(pass_context=True, name="foff", aliases=["fuckoff"])
     async def _foff(self, ctx, user: discord.Member):
         """Displays a random fuck off message."""
         await ctx.send("{} {}.\n\nBy yours truly {}.".format(randchoice(self.foffmsg), user.mention, ctx.message.author.mention))
 
-    @vbu.commands(pass_context=True, name="afoff", aliases=["anonymousfoff", "afuckoff", "anonymousfuckoff"])
+    @vbu.command(pass_context=True, name="afoff", aliases=["anonymousfoff", "afuckoff", "anonymousfuckoff"])
     async def _afoff(self, ctx, user: discord.Member):
         """Displays an anonymous random fuck off message."""
         try:
@@ -26,12 +26,12 @@ class fuckoff(vbu.Cog):
             pass
         await ctx.send("{} {}. ".format(randchoice(self.foffmsg), user.mention))
 
-    @vbu.commands(pass_context=True, name="ifoff", aliases=["itemfoff", "ifuckoff", "itemfuckoff"])
+    @vbu.command(pass_context=True, name="ifoff", aliases=["itemfoff", "ifuckoff", "itemfuckoff"])
     async def _ifoff(self, ctx, *, item):
         """Displays a random fuck off message for items."""
         await ctx.send("{} {}.\n\nBy yours truly {}.".format(randchoice(self.foffmsg), item, ctx.message.author.mention))
 
-    @vbu.commands(pass_context=True, name="iafoff", aliases=["itemafoff", "iafuckoff", "ianonymousfuckoff", "itemanonymousfoff", "itemanonymousfuckoff"])
+    @vbu.command(pass_context=True, name="iafoff", aliases=["itemafoff", "iafuckoff", "ianonymousfuckoff", "itemanonymousfoff", "itemanonymousfuckoff"])
     async def _aifoff(self, ctx, *, item: str):
         """Displays an anonymous random fuck off message for items."""
         try:
