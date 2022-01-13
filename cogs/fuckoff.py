bb73abde049383280265316fc1c5570139287f94
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-14 01:12:31 +1000

    Update fuckoff.py

--- cogs/fuckoff.py
+++ cogs/fuckoff.py
@@ -8,7 +8,7 @@ import discord.utils
 class fuckoff(commands.Cog):
     """Fuck off."""
 
-    def __init__(self, bot, msg):
+    def __init__(self, bot):
         self.bot = bot
         self.foffmsg = ["Locating a fuck to give", "Looking for a fuck to give", "It is called KARMA and it is pronounced as haha fuck you", "You cannot imagine the immensity of the fuck I do not give", "I dont hate you but, lets put it this way. If I had a bucket of water and you were on fire, I would drink the bucket of water", "I hope you step on a lego", "Even with this wine you look ugly", "NO, FUCK YOU", "Dear karma, I have a list of people you missed. One of them is", "Attempting to give a fuck \n`PLEASE WAIT`"]
 
@@ -18,7 +18,7 @@ class fuckoff(commands.Cog):
         await ctx.send("{} {}.\n\nBy yours truly {}.".format(randchoice(self.foffmsg), user.mention, ctx.message.author.mention))
 
     @commands.command(pass_context=True, name="afoff", aliases=["anonymousfoff", "afuckoff", "anonymousfuckoff"])
-    async def _afoff(self, ctx, user: discord.Member):
+    async def _afoff(self, ctx, msg, user: discord.Member):
         """Displays an anonymous random fuck off message."""
         try:
             await self.msg.delete(ctx.message)
@@ -32,7 +32,7 @@ class fuckoff(commands.Cog):
         await ctx.send("{} {}.\n\nBy yours truly {}.".format(randchoice(self.foffmsg), item, ctx.message.author.mention))
 
     @commands.command(pass_context=True, name="iafoff", aliases=["itemafoff", "iafuckoff", "ianonymousfuckoff", "itemanonymousfoff", "itemanonymousfuckoff"])
-    async def _aifoff(self, ctx, *, item: str):
+    async def _aifoff(self, ctx, msg, *, item: str):
         """Displays an anonymous random fuck off message for items."""
         try:
             await self.msg.delete(ctx.message)
