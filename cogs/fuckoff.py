211fbbb60d4340071303d43a94547d08b0edb8cd
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-14 00:49:14 +1000

    Update fuckoff.py

--- cogs/fuckoff.py
+++ cogs/fuckoff.py
@@ -32,7 +32,7 @@ class fuckoff(commands.Cog):
         await ctx.send("{} {}.\n\nBy yours truly {}.".format(randchoice(self.foffmsg), item, ctx.message.author.mention))
 
     @commands.command(pass_context=True, name="iafoff", aliases=["itemafoff", "iafuckoff", "ianonymousfuckoff", "itemanonymousfoff", "itemanonymousfuckoff"])
-    async def _afoff(self, ctx, *, item: str):
+    async def _aifoff(self, ctx, *, item: str):
         """Displays an anonymous random fuck off message."""
         try:
             await self.bot.delete_message(ctx.message)
