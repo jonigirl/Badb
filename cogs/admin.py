6019fabcb3f0bb2f0d7461389889d8a014bc9222
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-17 05:38:53 +1000

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,8 +2,8 @@
 import discord
 from discord.ext import commands, vbu
 
-from .utils import checks, quickembed
-from .utils.fjclasses import DiscordUser
+from discord.utils import checks, quickembed
+from discord.utils.fjclasses import DiscordUser
 
 
 class Admin(vbu.Cog):
