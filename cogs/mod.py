b696eef48a02709961323078e4be5db354439f4b
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-17 11:31:05 +1000

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,6 +1,6 @@
 import discord
 from discord import utils
-from discord.ext import commands,  # vbu, send_cmd_help, settings
+from discord.ext import commands  # vbu, send_cmd_help, settings
 # from vbu import utils
 from .utils.dataIO import fileIO
 from .utils import checks
