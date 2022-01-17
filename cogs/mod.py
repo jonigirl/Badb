1a257438e2644b1653e7c9018500c54bb11fed92
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-17 11:24:39 +1000

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -3,7 +3,7 @@ from discord import utils
 from discord.ext import vbu
 # from utils.dataIO import fileIO
 # from utils import checks
-from __main__ import send_cmd_help, settings
+from vbu import send_cmd_help, settings
 import os
 import logging
 
