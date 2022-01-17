111d843874a05e8bef886db59bfa91dbaa2adba4
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-17 12:53:47 +1000

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -3,7 +3,7 @@ from dataIO import fileIO
 from discord.ext import commands, vbu
 import discord.utils
 from cogs.utils import checks
-from cogs.utils.dataIO import fileIO
+from cogs.dataIO import fileIO
 from __main__ import settings
 
 # from utils import checks
