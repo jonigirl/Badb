7a9433e5684a295bfc9f2df2cb9bce674b8d84f7
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-17 15:02:50 +1000

    Update checks.py

--- cogs/checks.py
+++ cogs/checks.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord.utils
-from cogs.utils.settings import Settings
-from cogs.utils.dataIO import fileIO
+from utils.settings import Settings
+from utils.dataIO import fileIO
 from __main__ import settings
 
 #
