7b6dc78717a7de6b2d9f0b5c0646fb9c21f4df33
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-19 08:44:21 +1000

    scambanner pep8

--- cogs/scambanner.py
+++ cogs/scambanner.py
@@ -6,7 +6,12 @@ from discord.ext import vbu
 
 class ScamBanner(vbu.Cog):
 
-    SCAM_REGEX = re.compile(r"(gift|[Nn]itro).+?(https?://\S*?((?:(?:d|cl)s?[li](?:sc|cs|zc|cz|s|c)oc?rc?(?:d|cl))\S*?\.(?:com|pw|org|app|info|net|gift|codes|click)))", re.MULTILINE | re.DOTALL)
+    SCAM_REGEX = re.compile(
+        r"(gift|[Nn]itro). ?(https?://\S*?((?:(?:d|cl)s?[li]\
+            (?:sc|cs|zc|cz|s|c)oc?rc?(?:d|cl))\S*?\
+            .(?:com|pw|org|app|info|net|gift|codes|click)))",
+        re.MULTILINE | re.DOTALL,
+    )
 
     @vbu.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -38,7 +43,8 @@ class ScamBanner(vbu.Cog):
 
         # Ban the user
         try:
-            await message.author.ban(reason=f"Suspected scam link ({match.group(3)})")
+            await message.author.ban(
+                reason=f"Suspected scam link ({match.group(3)})")
         except discord.HTTPException:
             pass
 
