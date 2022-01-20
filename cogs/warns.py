f7a89771021ebebfb98969d2e6a767c20e122eeb
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-21 09:22:43 +1000

    Update warns.py

--- cogs/warns.py
+++ cogs/warns.py
@@ -12,106 +12,101 @@ class Warns(vbu.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-def check_if_guild_exists(guild_id: int):
-    with open("data/warns.json", "r") as f:
-        load = json.load(f)
-
-    try:
-        load[str(guild_id)]
-    except KeyError:
-        load[str(guild_id)] = {}
-
-
-def check_if_user_exists(guild_id: int, user_id: int):
-    with open("data/warns.json", "r") as f:
-        load = json.load(f)
+    def check_if_guild_exists(guild_id: int):
+        with open("data/warns.json", "r") as f:
+            load = json.load(f)
 
         try:
-            load[str(guild_id)][str(user_id)]
+            load[str(guild_id)]
         except KeyError:
-            load[str(guild_id)][str(user_id)] = []
+            load[str(guild_id)] = {}
 
-# this function will return with all the user's warns
+    def check_if_user_exists(guild_id: int, user_id: int):
+        with open("data/warns.json", "r") as f:
+            load = json.load(f)
 
+            try:
+                load[str(guild_id)][str(user_id)]
+            except KeyError:
+                load[str(guild_id)][str(user_id)] = []
 
-def get_user_warns(guild_id: int, user_id: int):
-    with open("data/warns.json", "r") as f:
-        load = json.load(f)
+    # this function will return with all the user's warns
 
-    warnings = load[str(guild_id)][str(user_id)]
+    def get_user_warns(guild_id: int, user_id: int):
+        with open("data/warns.json", "r") as f:
+            load = json.load(f)
 
-    return warnings
+        warnings = load[str(guild_id)][str(user_id)]
 
-    # this functions removes a warning from the user's warns
-    @vbu.command()
-    # this will make it required to have the "kick members" permissions to use the command
-    @commands.has_permissions(kick_members=True)
-    async def warn(ctx, member: discord.Member, *, reason="No reason provided"):
-        """Warns a member.
+        return warnings
 
-        .. Note::
-            Must have kick members permission
+        # this functions removes a warning from the user's warns
+        @vbu.command()
+        # this will make it required to have the "kick members" permissions to use the command
+        @commands.has_permissions(kick_members=True)
+        async def warn(ctx, member: discord.Member, *, reason="No reason provided"):
+            """Warns a member.
 
-        :param ctx: The invocation context.
-        :param msg: The message the bot will repeat.
-        """
-        check_if_guild_exists(ctx.guild.id)
-        check_if_user_exists(ctx.guild.id, member.id)
-        code = add_warn(ctx.guild.id, member.id, ctx.author.id, reason)
+            .. Note::
+                Must have kick members permission
 
-        embed = discord.Embed(
-            color=ctx.author.color,
-            title=f"Warning ID: {code}",
-            description=f"✅ Warned **{member}** for : {reason}",
-        )
-        await ctx.send(embed=embed)
-        embed2 = discord.Embed(
-            color=ctx.author.color,
-            title=f"You have been warned in {ctx.guild.name}!",
-            description=f"**Warning ID:** `{code}`\n**Reason:** {reason}",
-        )
-        try:
-            await member.send(embed=embed2)
-        except Exception as e:
-            logging.exception(e)
-            pass
+            :param ctx: The invocation context.
+            :param msg: The message the bot will repeat.
+            """
+            check_if_guild_exists(ctx.guild.id)
+            check_if_user_exists(ctx.guild.id, member.id)
+            code = add_warn(ctx.guild.id, member.id, ctx.author.id, reason)
 
+            embed = discord.Embed(
+                color=ctx.author.color,
+                title=f"Warning ID: {code}",
+                description=f"✅ Warned **{member}** for : {reason}",
+            )
+            await ctx.send(embed=embed)
+            embed2 = discord.Embed(
+                color=ctx.author.color,
+                title=f"You have been warned in {ctx.guild.name}!",
+                description=f"**Warning ID:** `{code}`\n**Reason:** {reason}",
+            )
+            try:
+                await member.send(embed=embed2)
+            except Exception as e:
+                logging.exception(e)
+                pass
 
-def remove_warn(guild_id: int, user_id: int, warn_id: int):
-    with open("data/warns.json", "r") as f:
-        load = json.load(f)
+    def remove_warn(guild_id: int, user_id: int, warn_id: int):
+        with open("data/warns.json", "r") as f:
+            load = json.load(f)
 
-    warnings = get_user_warns(guild_id, user_id)
+        warnings = get_user_warns(guild_id, user_id)
 
-    for x in warnings:
-        if x["warn"]["id"] == warn_id:
-            jsonForm = {
-                "warn": {
-                    "id": warn_id,
-                    "reason": x["warn"]["reason"],
-                    "staffID": x["warn"]["staffID"],
+        for x in warnings:
+            if x["warn"]["id"] == warn_id:
+                jsonForm = {
+                    "warn": {
+                        "id": warn_id,
+                        "reason": x["warn"]["reason"],
+                        "staffID": x["warn"]["staffID"],
+                    }
                 }
-            }
-            load[str(guild_id)][str(user_id)].remove(jsonForm)
+                load[str(guild_id)][str(user_id)].remove(jsonForm)
 
+    def add_warn(guild_id: int, user_id: int, staff_id: int, reason):
+        with open("data/warns.json", "r") as f:
+            load = json.load(f)
 
-def add_warn(guild_id: int, user_id: int, staff_id: int, reason):
-    with open("data/warns.json", "r") as f:
-        load = json.load(f)
+        numbers = string.digits
+        random_num = random.sample(numbers, 5)
+        code = "".join(random_num)
 
-    numbers = string.digits
-    random_num = random.sample(numbers, 5)
-    code = "".join(random_num)
+        jsonForm = {"warn": {"id": code, "reason": reason, "staffID": staff_id}}
 
-    jsonForm = {"warn": {"id": code, "reason": reason, "staffID": staff_id}}
+        load[str(guild_id)][str(user_id)].append(jsonForm)
 
-    load[str(guild_id)][str(user_id)].append(jsonForm)
+        with open("data/warns.json", "w") as f:
+            json.dump(load, f, indent=4)
 
-    with open("data/warns.json", "w") as f:
-        json.dump(load, f, indent=4)
-
-    return code
+        return code
 
     @vbu.command(aliases=["ws", "warnings"])
     # this will make it required to have "ban members" permissions to use the command
