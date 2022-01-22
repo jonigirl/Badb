4f184acb173ad1d0731f48bf1e9f647af4677199
Joni Hayes <40481310+jonigirl@users.noreply.github.com>
2022-01-23 01:34:43 +1000

    update scanbanner
    add updates to scambanner from 
    Voxel-Fox_Ltd 18th

--- cogs/scambanner.py
+++ cogs/scambanner.py
@@ -4,23 +4,30 @@ import discord
 
 from discord.ext import vbu
 
+SCAM_REGEX = re.compile(
+    r"""
+        ([Gg][Ii][Ff][Tt]|[Nn][Ii][Tt][Rr][Oo]|[Aa]irdrop|@everyone)
+        .+?
+        (
+            (https?://)(\S*?)
+            ((?:d|cl)s?[li](?:sc|cs|zc|cz|s|c|sck)r?oc?r?c?(?:d|cl)?s?)
+            (\S*?)\.
+            (com|pw|org|app|info|net|gift|codes|click|club)
+        )
+    """,
+    re.MULTILINE | re.DOTALL | re.VERBOSE,
+)
+
 
 class ScamBanner(vbu.Cog):
-
-    SCAM_REGEX = re.compile(
-        r"(gift|[Nn]itro). ?(https?://\S*?((?:(?:d|cl)s?[li]\
-            (?:sc|cs|zc|cz|s|c)oc?rc?(?:d|cl))\S*?\
-            .(?:com|pw|org|app|info|net|gift|codes|click)))",
-        re.MULTILINE | re.DOTALL,
-    )
-
+        
     @vbu.Cog.listener()
     async def on_message(self, message: discord.Message):
 
         # Ignore DMs
         if message.guild is None:
             return
-        if message.guild.id != 208895639164026880:
+        if message.guild.id != 738753442461253633:
             return
 
         # Ignore people with roles
@@ -29,7 +36,7 @@ class ScamBanner(vbu.Cog):
             return
 
         # See if we match
-        match = self.SCAM_REGEX.search(message.content)
+        SCAM_REGEX.search(message.content)
         if not match:
             return
 
@@ -38,8 +45,9 @@ class ScamBanner(vbu.Cog):
             "discord.gift",
             "discordapp.com",
             "discord.com",
+            "discord.gg",
         ]
-        if match.group(3).lower() in valid_links:
+        if match.group(4).lower() in valid_links:
             return
 
         # Ban the user
